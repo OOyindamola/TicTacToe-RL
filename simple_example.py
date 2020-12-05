f95ef8baaa0f96cdbfe3e9d1d4198522d56f4417
ancientronin <roblewis8855@gmail.com>
2020-12-05 16:36:49 -0500

    Update simple_example.py

--- simple_example.py
+++ simple_example.py
@@ -25,52 +25,17 @@ def Build_MDP():
     return P
 
 
-def no_pol(s,a):
-    return 1
-
-def p1(s,a,A):
-    tot = 0
-    for As in A:
-        tot = tot + hlp(s,As)
-    tot = 1/tot
-
-    return hlp(s,a)*tot
-
-def get_blank_pol():
-    return {s:'u' for s in S}
-
-def get_blank_pol2():
-    return {s:1 for s in S}
-
-def const_pol_hlp(c,s):
-    new_s = act(s,c)
-    for ss in new_s:
-        if ss<0 or ss >= size:
-            return 0
-    return 1
-
-def const_pol(c):
-    def pol(s,a,A):
-        ans = 1 if a==c else 0
-        return ans * const_pol_hlp(c,s)
-    return pol
-
-def determine(pi):
-    X = [ rand_select(pi,s) for s in S ]
-    return X
-
 def Q_determine(pi):
     X = [ Q_select(pi,s) for s in S ]
     return X
 
 def Qdisp(Q):
     display2([Q[(s,Q_select(Q,s))] for s in S])
-
 def Q_select( Q, s ):
     x = ra.random()
     if x < .5:
         return A[int(ra.random()*len(A))]
-    
+
     maxv=-999999
     maxa='error'
     for a in A:
@@ -80,28 +45,27 @@ def Q_select( Q, s ):
             maxa = a
     return maxa
 
-
 def DQ(P, s0=(0,0), rate=.8):
     NSA = {(s,a):0 for s in S for a in A}
-    Q = [{ (s,a):0 for s in S for a in A },{ (s,a):0 for s in S for a in A }] 
+    Q = [{ (s,a):0 for s in S for a in A },{ (s,a):0 for s in S for a in A }]
     for i in range(10000):
         s = s0
         gameover = False
         while not gameover:
             QAB = { (s,a): Q[0][(s,a)] + Q[1][(s,a)] for a in A }
             a = Q_select( QAB, s )
-            
-            x = int(ra.random()*2) % 2
+
+x = int(ra.random()*2) % 2
             QA, QB = Q[x], Q[1-x]
-            
+
             #a = Q_select(QB, s)
             s2,r = P(s,a)
-            
+
             NSA[(s,a)] = NSA[(s,a)] + 1
-            rate = 1/ np.pow(NSA[(s,a)], .8)
+            rate = 1/ pow(NSA[(s,a)], .8)
 
             gameover = (s2=='C' or s2=='D')
-            
+
 
 
             a_star = Q_select(QA, s2)
@@ -109,7 +73,7 @@ def DQ(P, s0=(0,0), rate=.8):
 
             s = s2
 
-        
+
     print('\n--------------\n\nQ0:')
     print(Q[0])
     #Qdisp(Q[0])
@@ -125,3 +89,4 @@ def DQ(P, s0=(0,0), rate=.8):
 
 P = Build_MDP()
 DQ(P, 'A')
+                                                                                                                            92,1          Bot
