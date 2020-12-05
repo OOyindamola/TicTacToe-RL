88612019f593f093f54192feb91ad4557199fe52
ancientronin <roblewis8855@gmail.com>
2020-12-05 17:02:49 -0500

    changing stuff

--- simple_example.py
+++ simple_example.py
@@ -8,7 +8,6 @@ GAMMA = float(input())
 S = ['A', 'B', 'C', 'D']
 A = ['l', 'r']
 
-
 def Build_MDP():
     def P(s,a):
         if s=='A':
@@ -31,11 +30,12 @@ def Q_determine(pi):
 
 def Qdisp(Q):
     display2([Q[(s,Q_select(Q,s))] for s in S])
+
 def Q_select( Q, s ):
     x = ra.random()
     if x < .5:
         return A[int(ra.random()*len(A))]
-
+    
     maxv=-999999
     maxa='error'
     for a in A:
@@ -45,35 +45,41 @@ def Q_select( Q, s ):
             maxa = a
     return maxa
 
-def DQ(P, s0=(0,0), rate=.8):
+def DQ(P, s0=(0,0), rate=.1, DoubleQ=True):
     NSA = {(s,a):0 for s in S for a in A}
-    Q = [{ (s,a):0 for s in S for a in A },{ (s,a):0 for s in S for a in A }]
-    for i in range(10000):
+    Q = [{ (s,a):0 for s in S for a in A },{ (s,a):0 for s in S for a in A }] 
+    for i in range(100000):
         s = s0
         gameover = False
         while not gameover:
-            QAB = { (s,a): Q[0][(s,a)] + Q[1][(s,a)] for a in A }
-            a = Q_select( QAB, s )
-
-x = int(ra.random()*2) % 2
+            
+            x = int(ra.random()*2) % 2
+            x = x * (1 if DoubleQ else 0)
             QA, QB = Q[x], Q[1-x]
-
+            
+            QAB = { (s,a): Q[0][(s,a)] + Q[1][(s,a)] for a in A }
+        
+            a = Q_select( QAB if DoubleQ else QA, s )
+            
             #a = Q_select(QB, s)
             s2,r = P(s,a)
-
+            
             NSA[(s,a)] = NSA[(s,a)] + 1
             rate = 1/ pow(NSA[(s,a)], .8)
 
             gameover = (s2=='C' or s2=='D')
-
+            
 
 
             a_star = Q_select(QA, s2)
+            if DoubleQ:
+                QB = QA
+
             QA[(s,a)] = QA[(s,a)] + rate * (r + GAMMA*QB[(s2,a_star)] - QA[(s,a)])
 
             s = s2
 
-
+        
     print('\n--------------\n\nQ0:')
     print(Q[0])
     #Qdisp(Q[0])
@@ -88,5 +94,4 @@ x = int(ra.random()*2) % 2
 
 
 P = Build_MDP()
-DQ(P, 'A')
-                                                                                                                            92,1          Bot
+DQ(P, 'A', DoubleQ=True)
